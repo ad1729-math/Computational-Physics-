2ae78205084ccf5eadd3df5245a5cabcf6e2f38a
ad1729-math <adas2001.suri@gmail.com>
2023-02-16 23:39:48 +0530

    Changes

---
+++ Assignment-1/Problem-1.py
@@ -0,0 +1,108 @@
+import numpy as np 
+import math as m 
+import scipy.special as sp 
+import matplotlib.pyplot as plt 
+
+#Up method 
+
+def j0(x):
+    return m.sin(x)/x 
+
+def j1(x):
+    return (m.sin(x)-x*m.cos(x))/x**2
+
+X=np.arange(0.1,50,0.2)
+J10=[]
+
+for x in X:
+    a=j0(x)
+    b=j1(x)
+    for i in range(1,10):
+        temp=b
+        b=(2*i+1)*b/x-a
+        a=temp 
+    J10.append(b)
+
+
+#Down method: Here we start from J50, J49 to which we assign some random value 
+
+#First we take a guess and scale as per the output  
+
+def j100(x):
+    return 1
+
+def j99(x):
+    return 1
+
+J1_test=[]
+for x in X: 
+    a=j100(x)
+    b=j99(x)
+    for i in range(99,1,-1):
+        temp=b
+        b=(2*i+1)*b/x-a
+        a=temp 
+    J1_test.append(b)
+
+S=((np.sin(X)-X*np.cos(X))/X**2)/J1_test
+
+#After we find the required scaling we use this as the funtion j50 and j49
+l=10 #l is the order of the Bessel funtion that we want to find, here l=10
+
+def j100(x):
+    i=int((x-0.1)/0.2)
+    return S[i]
+
+def j99(x):
+    return j100(x)
+
+Jl=[]
+for x in X: 
+    a=j100(x)
+    b=j99(x)
+    for i in range(99,l,-1):
+        temp=b
+        b=(2*i+1)*b/x-a
+        a=temp 
+    Jl.append(b)
+
+
+e10=[]
+for x in X:
+    i=int((x-0.1)/0.2)
+    v=(abs(J10[i]-Jl[i]))/(abs(J10[i])+abs(Jl[i]))
+    e10.append(v)
+
+#print(e10) #This prints the relative error for all points of the given range
+
+plt.subplot(2,2,1)
+plt.plot(X,J10,'g',label="$j_{10}$ by up method")
+plt.plot(X,X*0,'r')
+#plt.plot(X,sp.spherical_jn(10,X)+0.01,'b+', label="Actual $j_{10}$")
+plt.xlim(1,50)
+plt.ylim(-0.5,0.5)
+plt.legend()
+
+
+plt.subplot(2,2,2)
+plt.plot(X,Jl,'r',label="$j_{10}$ by down method, starting from $j_{100}, j_{99}$")
+#plt.plot(X,sp.spherical_jn(10,X),'g+',label="Actual $j_1$")
+plt.plot(X,X*0,'k')
+plt.xlim(0.1,50)
+plt.ylim(-0.3,0.3)
+plt.legend()
+
+
+plt.subplot(2,2,3)
+plt.loglog(X,J10)  
+plt.loglog(X,Jl,"+")
+plt.legend(["log-log plot for up $j_{10}$   ","log-log plot for down $j_{10}$"])
+
+
+plt.subplot(2,2,4)
+plt.loglog(X,e10,'b',label="log-log plot of the error")
+#plt.ylim(-1,1)
+plt.legend()
+plt.show() 
+
+
---
+++ Assignment-1/Problem-2.py
@@ -0,0 +1,49 @@
+#Derivative of the arctan function 
+
+import math as m 
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+h=np.linspace(10**-10,10,10**5) #This is the step length 
+h1=np.linspace(10**-10,0.01,10**6)
+a=m.sqrt(2)
+
+def f2c(h):
+    return (np.arctan(a+h)-np.arctan(a))/h
+
+def f3c(h):
+     
+    return (np.arctan(a+h)-np.arctan(a-h))/(2*h)
+    
+
+
+#Comments: We can see that the 3 point method approaches the actual value much faster than 2 point method when we decrease the 
+#value of h. Also we can see from the plot that the deviation of 3 point method from the actual value 1/3 is quadratic (O(h^2))
+#while that the deviation of 2 point method from the actual value is apporximately linear (O(h)). 
+
+
+
+#Logarithmic scale error plot:
+
+def E1(h):
+    v=abs(3*(1/3-(np.arctan(a+h)-np.arctan(a))/h))
+    return np.log10(v)
+
+def E2(h):
+    v=abs(3*(1/3-(np.arctan(a+h)-np.arctan(a-h))/(2*h)))
+    return np.log10(v)
+
+plt.subplot(1,2,1)
+plt.plot(h,f2c(h),'r',label="$f'_{2c}$")
+plt.plot(h,f3c(h),'g',label="$f'_{3c}$")
+plt.plot(h,h/h*1/3,'k',label="$f'=1/3$")
+plt.legend()
+plt.subplot(1,2,2)
+plt.plot(np.log10(h1),E1(h1),'r',label="Logscale deviation for $f'_{2c}$")
+plt.plot(np.log10(h1),E2(h1),'g',label="Logscale deviation for $f'_{3c}$")
+plt.legend()
+plt.show() 
+
+
+
+
