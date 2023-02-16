bf8c1c2d509b3a80e8eff440a042363a6c16a84d
Alapan Das <82791449+ad1729-math@users.noreply.github.com>
2023-02-16 20:38:18 +0530

    Add files via upload

--- Assignment-1/Problem-1.py
+++ Assignment-1/Problem-1.py
@@ -28,17 +28,17 @@ for x in X:
 
 #First we take a guess and scale as per the output  
 
-def j50(x):
+def j100(x):
     return 1
 
-def j49(x):
+def j99(x):
     return 1
 
 J1_test=[]
 for x in X: 
-    a=j50(x)
-    b=j49(x)
-    for i in range(49,1,-1):
+    a=j100(x)
+    b=j99(x)
+    for i in range(99,1,-1):
         temp=b
         b=(2*i+1)*b/x-a
         a=temp 
@@ -46,36 +46,53 @@ for x in X:
 
 S=((np.sin(X)-X*np.cos(X))/X**2)/J1_test
 
-def j50(x):
+#After we find the required scaling we use this as the funtion j50 and j49
+l=10 #l is the order of the Bessel funtion that we want to find, here l=10
+
+def j100(x):
     i=int((x-0.1)/0.2)
     return S[i]
 
-def j49(x):
-    return j50(x)
+def j99(x):
+    return j100(x)
 
-J1=[]
+Jl=[]
 for x in X: 
-    a=j50(x)
-    b=j49(x)
-    for i in range(49,1,-1):
+    a=j100(x)
+    b=j99(x)
+    for i in range(99,l,-1):
         temp=b
         b=(2*i+1)*b/x-a
         a=temp 
-    J1.append(b)
+    Jl.append(b)
 
-plt.subplot(1,2,1)
+
+e10=[]
+for x in X:
+    i=int((x-0.1)/0.2)
+    v=(abs(J10[i]-Jl[i]))/(abs(J10[i])+abs(Jl[i]))
+    e10.append(v)
+
+plt.subplot(2,2,1)
 plt.plot(X,J10,'g',label="$j_{10}$ by up method")
 plt.plot(X,X*0,'r')
-plt.plot(X,sp.spherical_jn(10,X)+0.01,'b', label="Actual $j_{10}$, shifted little bit for distinguishability")
+#plt.plot(X,sp.spherical_jn(10,X)+0.01,'b', label="Actual $j_{10}$, shifted little bit for distinguishability")
 plt.xlim(1,50)
 plt.ylim(-0.5,0.5)
 plt.legend()
-plt.subplot(1,2,2)
-plt.plot(X,J1,'r',label="$j_1$ by down method, starting from $j_{50}, j_{49}$")
-plt.plot(X,sp.spherical_jn(1,X),'g+',label="Actual $j_1$, shifted little bit for distinguishability")
+plt.subplot(2,2,2)
+plt.plot(X,Jl,'r',label="$j_1$ by down method, starting from $j_{50}, j_{49}$")
+#plt.plot(X,sp.spherical_jn(1,X),'g+',label="Actual $j_1$, shifted little bit for distinguishability")
 plt.plot(X,X*0,'k')
 plt.xlim(0.1,50)
-plt.ylim(-10,10)
+plt.ylim(-0.3,0.3)
+plt.legend()
+plt.subplot(2,2,3)
+plt.loglog(X,J10,Jl)
+plt.legend(["log-log plot for up $j_{10}$   ","log-log plot for down $j_{10}$"])
+plt.subplot(2,2,4)
+plt.loglog(X,e10,'b',label="log-log plot of the error")
+#plt.ylim(-1,1)
 plt.legend()
 plt.show() 
 
