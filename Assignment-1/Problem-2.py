bf53d532b96c5efc099ea6aad3fd68f742fc5d65
Alapan Das <82791449+ad1729-math@users.noreply.github.com>
2023-02-16 20:49:03 +0530

    Add files via upload

--- Assignment-1/Problem-2.py
+++ Assignment-1/Problem-2.py
@@ -4,7 +4,8 @@ import math as m
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-h=np.linspace(10**-2,1,1000) #This is the step length 
+h=np.linspace(10**-10,10,10**5) #This is the step length 
+h1=np.linspace(10**-10,0.01,10**6)
 a=m.sqrt(2)
 
 def f2c(h):
@@ -25,7 +26,7 @@ def f3c(h):
 #Logarithmic scale error plot:
 
 def E1(h):
-    v=abs(3*(1/3-(np.arctan(a+h)-np.arctan(h))/h))
+    v=abs(3*(1/3-(np.arctan(a+h)-np.arctan(a))/h))
     return np.log10(v)
 
 def E2(h):
@@ -38,8 +39,8 @@ plt.plot(h,f3c(h),'g',label="$f'_{3c}$")
 plt.plot(h,h/h*1/3,'k',label="$f'=1/3$")
 plt.legend()
 plt.subplot(1,2,2)
-plt.plot(np.log10(h),E1(h),'r',label="Logscale deviation for $f'_{2c}$")
-plt.plot(np.log10(h),E2(h),'g',label="Logscale deviation for $f'_{3c}$")
+plt.plot(np.log10(h1),E1(h1),'r',label="Logscale deviation for $f'_{2c}$")
+plt.plot(np.log10(h1),E2(h1),'g',label="Logscale deviation for $f'_{3c}$")
 plt.legend()
 plt.show() 
 
