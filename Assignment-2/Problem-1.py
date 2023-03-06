ef706c6baaaea0d2dc5c359bd2637677b780c2a6
Alapan Das <82791449+ad1729-math@users.noreply.github.com>
2023-03-06 20:46:29 +0530

    Add files via upload

---
+++ Assignment-2/Problem-1.py
@@ -0,0 +1,89 @@
+import parser
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+#Defining the Matrix 
+N=400
+h=1/(N-1)
+
+A1=[1]
+for j in range(1,N):
+    A1.append(0)
+
+L=[A1]
+
+for i in range(1,N-1):
+    S=[]
+    for j in range(0,N):
+        if abs(i-j)==1:
+            S.append(1)
+        else: 
+            if j==i:
+                S.append(-2) 
+            else: 
+                S.append(0)
+    L.append(S)
+
+AN=[]
+for i in range(0,N-1):
+    AN.append(0)
+AN.append(1) 
+
+L.append(AN)
+
+#Defining the vector 
+
+def f(x):
+    return (3*x+x**2)*np.exp(x)
+
+U=[]
+for i in range(0,N-1):
+    x=(i)*h
+    U.append(-(h**2)*f(x))
+U.append(0)
+
+#Gauss Elimination
+M=L
+Y=U
+n=len(M)
+
+
+
+for i in range(0,n):
+    v=M[i][i]
+    M[i]=[x/v for x in M[i]] 
+    Y[i]=Y[i]/v 
+    for j in range(i+1, n):
+        a=M[j][i]
+        Temp=[]
+        for k in range(0,n):
+            t=M[j][k]-M[i][k]*a
+            Temp.append(t)
+        M[j]=Temp
+        Y[j]=Y[j]-Y[i]*a
+
+S=[Y[n-1]]
+
+for k in range(n-2,-1,-1):
+    s=Y[k]
+    for j in range(n-1,k,-1):
+        s-=S[n-1-j]*M[k][j]
+    S.append(s) 
+
+S.reverse()
+
+#print(np.array(M),Y,S)
+
+def Uact(x):
+    return x*(1-x)*np.exp(x)
+    
+Err=[]
+for k in range(len(M)):
+    c=abs((S[k]-Uact(k*h))/Uact(k*h))
+    Err.append(c)    
+
+X=np.linspace(0, 1, N)
+plt.plot(X, S,'k+',label="Numerically found value")
+plt.plot(X, Uact(X),'r',label="$u^{exact}$")
+plt.legend()
+plt.show()
