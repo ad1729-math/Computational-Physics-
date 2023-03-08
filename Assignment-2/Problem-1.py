0030d42c147bead31f74d18246d4b3156b73f8c6
Alapan Das <82791449+ad1729-math@users.noreply.github.com>
2023-03-08 20:38:50 +0530

    Add files via upload

--- Assignment-2/Problem-1.py
+++ Assignment-2/Problem-1.py
@@ -1,9 +1,8 @@
-import parser
 import numpy as np 
 import matplotlib.pyplot as plt 
 
 #Defining the Matrix 
-N=10**4
+N=5000
 h=1/(N-1)
 
 A1=[1]
@@ -53,21 +52,19 @@ for i in range(0,n-1):
     v=M[i][i]
     M[i]=[x/v for x in M[i]] 
     Y[i]=Y[i]/v 
-    for j in range(i+1, n-1):
-        a=M[j][i]
-        for k in range(j-1,j+2):
-            M[j][k]=M[j][k]-M[i][k]*a
-        Y[j]=Y[j]-Y[i]*a
+    a=M[i+1][i]
+    for k in range(i,i+2):
+        M[i+1][k]=M[i+1][k]-M[i][k]*a
+    Y[i+1]=Y[i+1]-Y[i]*a
 
 
 #Backward substitution 
 
-for i in range(n-1,-1,-1):
-    for j in range(i-1,-1,-1):
-        for k in range(j-1,j+2):
-            a=M[j][i]
-            M[j][k]=M[j][k]-M[i][k]*a
-        Y[j]=Y[j]-Y[i]*a
+for i in range(n-1,0,-1):
+    a=M[i-1][i]
+    for k in range(i-1,i+1):
+        M[i-1][k]=M[i-1][k]-M[i][k]*a
+    Y[i-1]=Y[i-1]-Y[i]*a
 
 #print(np.array(M),Y)
 
@@ -86,4 +83,15 @@ X=np.linspace(0, 1, N)
 plt.plot(X, Y,'k+',label="Numerically found value")
 plt.plot(X, Uact(X),'r',label="$u^{exact}$")
 plt.legend()
+
+#
+N=[100,250,500,750,1000,2000,2500,3000,4000,5000,7500,10000]
+E=[6.659835336247023e-05,1.0560407239650785e-05,2.6322226044365292e-06,1.1687127686445837e-06,6.570740200539755e-07,
+1.6414560411029998e-07,1.0503666113608006e-07,7.293369284391954e-08,4.1017049273798214e-08,2.6246275045706256e-08,
+1.1659389737943674e-08,6.5483221261819044e-09]
+
+#plt.plot(-np.log(N), np.log(E),'bo',label="Log plot of maximum errors")
+#plt.xlabel("$\log(h)$--->")
+#plt.ylabel("$\log(\epsilon)$--->")
+#plt.legend()
 plt.show()
