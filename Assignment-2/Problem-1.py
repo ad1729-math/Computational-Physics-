7f1e030529b4c415b16a1ee94ad1bf273089b361
Alapan Das <82791449+ad1729-math@users.noreply.github.com>
2023-03-09 23:04:13 +0530

    Add files via upload

--- Assignment-2/Problem-1.py
+++ Assignment-2/Problem-1.py
@@ -34,7 +34,7 @@ M=L
 Y=U
 n=len(M)
 
-#Backward substitution
+#Forward substitution
 M[1][0]-=M[0][0]
 v=M[1][1]
 M[1]=[x/v for x in M[1]]
@@ -48,7 +48,8 @@ for i in range(1,n-1):
     M[i+1][1]-=M[i][2]*a
     Y[i+1]-=Y[i]*a
 
-#Forward substitution 
+
+#Backward substitution 
 
 M[n-2][2]=0
 
@@ -56,6 +57,7 @@ for i in range(n-2,-1,-1):
     a=M[i-1][2]
     M[i-1][2]-=M[i][1]*a
     Y[i-1]-=Y[i]*a
+
 
 
 #print(np.array(M),Y)
@@ -69,7 +71,7 @@ for k in range(len(M)):
     Err.append(c)    
 
 Err_sort=sorted(Err) 
-print(Err_sort[-2])   
+#print(Err_sort[-2])   
 
 X=np.linspace(0, 1, N)
 plt.plot(X, Y,'k+',label="Numerically found value")
@@ -77,15 +79,20 @@ plt.plot(X, Uact(X),'r',label="$u^{exact}$")
 plt.legend()
 
 
-#Follwoing are the printed error values for different h
+#These values of maximum error have been found by inputting different values of N
 
-N=[100,250,500,750,1000,2000,2500,3000,4000,5000,7500,10**4,5*10**4,10**5,5*10**5,10**6]
-E=[6.659835336247023e-05,1.0560407239650785e-05,2.6322226044365292e-06,1.1687127686445837e-06,6.570740200539755e-07,
+N=[10,100,250,500,750,1000,2000,2500,3000,4000,5000,7500,10**4,5*10**4,10**5,5*10**5,10**6]
+E=[0.0076482162546865845,6.659835336247023e-05,1.0560407239650785e-05,2.6322226044365292e-06,1.1687127686445837e-06,6.570740200539755e-07,
 1.6414560411029998e-07,1.0503666113608006e-07,7.293369284391954e-08,4.1017049273798214e-08,2.6246275045706256e-08,
 1.1659389737943674e-08,6.5483221261819044e-09,5.73739581940873e-10,6.349200159782446e-10,3.234784674211385e-07,
 8.300185081108075e-07]
 
-#plt.plot(-np.log(N), np.log(E),'bo',label="Log plot of maximum errors")
+N1=[10,100,1000,10000,100000,1000000]
+E1=[E[N.index(x)] for x in N1]
+
+a=np.log(10)
+
+#plt.plot(-np.log(N1)/a, np.log(E1)/a,'bo',label="Log plot of maximum errors")
 #plt.xlabel("$\log(h)$--->")
 #plt.ylabel("$\log(\epsilon)$--->")
 #plt.legend()
