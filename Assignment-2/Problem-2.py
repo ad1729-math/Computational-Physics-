f15e37a957b4c344f723f30b0e13d2f3361ff08c
Alapan Das <82791449+ad1729-math@users.noreply.github.com>
2023-03-09 20:01:47 +0530

    Add files via upload

--- Assignment-2/Problem-2.py
+++ Assignment-2/Problem-2.py
@@ -3,66 +3,66 @@ import numpy as np
 I0=1
 R1,R2,R3,R4,R5=2,4,6,8,10
 
-M=[[1,0,0,1,0],
+Z=[[1,0,0,1,0],
    [0,1,1,0,0],
-   [0,R2,-R3,0,-R5],
-   [R1,0,0,-R4,R5],
    [1,-1,0,0,-1],
+   [R1,0,0,-R4,R5],
+   [0,R2,-R3,0,-R5],
    ]
 
 R=[I0,I0,0,0,0]
-n=len(M)
-
-U=M
-LT=[]
-for i in range(len(U)):
-   S=[]
-
-   for k in range(0,i):
-      S.append(0)
-   S.append(1)
-
-   for j in range(i+1,len(U)):
-      v=U[j][i]/U[i][i]
-
-      P=[]
-      for k in range(len(U)):
-         a=U[j][k]-U[i][k]*v
-         P.append(a)
-      U[j]=P
-
-      S.append(v)
-   LT.append(S)
-
+n=len(Z)
 
 L=[]
-for i in range(len(LT)):
+for i in range(len(Z)):
    S=[]
-   for j in range(len(LT)):
-      S.append(LT[j][i])
+   for j in range(len(Z)):
+      if i==j:
+         S.append(1)
+      else:
+         S.append(0)
    L.append(S)
 
 
+for i in range(len(Z)):
+
+   if Z[i][i]==0:
+      for k in range(i+1,len(Z)):
+         if Z[k][i]!=0:
+            Z[i], Z[k]=Z[k], Z[i]
+            R[i],R[k]=R[k],R[i]
+            for j in range(0,i):
+               L[i][j], L[k][j]= L[k][j], L[i][j]
+            break
+
+   for j in range(i+1,len(Z)):
+      v=Z[j][i]/Z[i][i]
+      for k in range(i,len(Z)):
+         Z[j][k]-=Z[i][k]*v 
+      L[j][i]=v
+         
+
+
 Y=[R[0]]
-for i in range(1,len(U)):
+for i in range(1,len(Z)):
    s=R[i]
    for j in range(0,i):
       s-=L[i][j]*Y[j]
    Y.append(s)
 
 
-S=[Y[n-1]/U[n-1][n-1]]
+S=[Y[n-1]/Z[n-1][n-1]]
 
 for k in range(n-2,-1,-1):
    s=Y[k]
    for j in range(n-1,k,-1):
-      s-=S[n-1-j]*U[k][j]
-   S.append(s/U[k][k]) 
+      s-=S[n-1-j]*Z[k][j]
+   S.append(s/Z[k][k]) 
 
 S.reverse()
 print("The LU decompoaistion of the matrix M is")
 print("L=",np.array(L))
-print("U=",np.array(U))
+print("U=",np.array(Z))
 print("Solution of LY=R is",Y)
 print("Solution of US=Y is",S)
 print("Hence I_1="+str(S[0]),"I2="+str(S[1]),"I3="+str(S[2]),"I4="+str(S[3]),"I5="+str(S[4]))
