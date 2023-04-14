a6405f5a26fef1ba31ee2681bb3b97db9b50d83c
Alapan Das <82791449+ad1729-math@users.noreply.github.com>
2023-04-14 21:51:10 +0530

    Add files via upload

---
+++ Assignment-4/Problem-1.py
@@ -0,0 +1,99 @@
+import numpy as np 
+from numpy import random 
+import matplotlib.pyplot as plt
+import math as m
+
+#Poisson distribution of random decay number
+
+def fact(n):
+    if n==0:
+        return 1
+    else:
+        p=1
+        for i in range(1,n+1):
+            p=p*i
+        return p
+
+def Poisson(n,l0):
+    return l0**n*m.exp(-l0)/fact(n)
+
+def Cu_Po(n,l):
+    if n==-1:
+        return 0
+    else:  
+       s=0
+       for i in range(0,n+1):
+            s+=Poisson(i,l)
+       return s
+
+def P_b(x,l):
+    for i in range(-1,20):
+        if Cu_Po(i,l)<x<Cu_Po(i+1,l):
+            return i+1
+        else:
+            continue
+
+def PP(l0):
+    x=np.random.rand()
+    return P_b(x,l0)
+
+#Decay
+N=1000
+dt=0.01
+l=0.3
+
+def Decay(n,dt):
+    l0=n*dt*l
+    return PP(l0)
+
+def Exp(N,dt):
+    i=0
+    T=[]
+    Dec=[]
+    Res=[]
+    while N>0:
+        T.append(i*dt)
+        s=Decay(N,dt)
+        if s<=N:
+           Dec.append(s)
+           N=N-s
+           Res.append(N)
+        else:
+            continue
+        i+=1
+    return T,Dec,Res
+
+
+T,Dec,Res=Exp(N,dt)
+plt.subplot(2,1,1)
+plt.plot(T,Dec,'ro',label="Number of decays")
+plt.xlabel("$t$(Time)--->")
+plt.ylabel("Number of decays")
+plt.legend()
+plt.subplot(2,1,2)
+plt.plot(T,Res,'bo',label="Number of esidues")
+plt.xlabel("$t$(Time)--->")
+plt.ylabel("Number of particles left--->")
+plt.legend()
+plt.show()
+
+
+#Ensemble avergae at time t
+def N(t,N0):
+    L=Exp(N0,dt)
+    Dec,Res=L[1],L[2]
+    i=int(t/dt)
+    if i<=len(Dec):
+       dN_t,N_t=Dec[i],Res[i]
+    else:
+       dN_t,N_t=0,0     
+    return dN_t,N_t 
+
+
+def En_av(t,N0,M):
+    s=0 
+    for i in range(M):
+        s+=N(t,N0)[1]
+    return s/M 
+
+print(En_av(1,1000,100))
