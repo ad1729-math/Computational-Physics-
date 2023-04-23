eec9397b1ed28e897a60fc7a931858e239670d1f
Alapan Das <82791449+ad1729-math@users.noreply.github.com>
2023-04-23 19:25:41 +0530

    Update Problem-1.py

--- Assignment-4/Problem-1.py
+++ Assignment-4/Problem-1.py
@@ -66,16 +66,16 @@ def Exp(N,dt):
 
 T,Dec,Res=Exp(N,dt)
 
-# plt.subplot(2,1,1)
-# plt.plot(T,Dec,'ro',label="Number of decays")
-# plt.xlabel("$t$(Time)--->")
-# plt.ylabel("Number of decays")
-# plt.legend()
-# plt.subplot(2,1,2)
-# plt.plot(T,Res,'bo',label="Number of esidues")
-# plt.xlabel("$t$(Time)--->")
-# plt.ylabel("Number of particles left--->")
-# plt.legend()
+plt.subplot(2,1,1)
+plt.plot(T,Dec,'ro',label="Number of decays")
+plt.xlabel("$t$(Time)--->")
+plt.ylabel("Number of decays")
+plt.legend()
+plt.subplot(2,1,2)
+plt.plot(T,Res,'bo',label="Number of esidues")
+plt.xlabel("$t$(Time)--->")
+plt.ylabel("Number of particles left--->")
+plt.legend()
 
 
 #Ensemble avergae at time t
@@ -107,18 +107,18 @@ s=0
 for i in range(len(T)):
     s+=(N_ln[i]-N_ln[-1])*dt 
 
-m=2*s/(T[-1]**2)
-print("The slope of the curve is",-m)
+m=-2*s/(T[-1]**2)
+print("The slope of the curve is",m)
 
-# plt.subplot(2,1,1)
-# plt.plot(T,N_ln,"ro",label="Log Ensemble average, $N_0$=1000")
-# plt.xlabel("$t$(Time)--->")
-# plt.ylabel("$\ln(N(t))$ (Log Ensemble average)--->")
-# plt.legend()
-# plt.subplot(2,1,2)
-# plt.plot(T,Sl,'g')
-# plt.xlabel("$t$ (Time)--->")
-# plt.ylabel("Slope--->")
-# plt.show()
+plt.subplot(2,1,1)
+plt.plot(T,N_ln,"ro",label="Log Ensemble average, $N_0$=1000")
+plt.xlabel("$t$(Time)--->")
+plt.ylabel("$\ln(N(t))$ (Log Ensemble average)--->")
+plt.legend()
+plt.subplot(2,1,2)
+plt.plot(T,Sl,'g')
+plt.xlabel("$t$ (Time)--->")
+plt.ylabel("Slope--->")
+plt.show()
 
 
